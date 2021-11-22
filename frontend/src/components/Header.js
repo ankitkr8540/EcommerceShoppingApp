61a3518f129f486f22e6395b873813f118983338
Ankit kumar <ankitkr8540@gmail.com>
2021-11-22 17:31:42 +0530

    refreshing of page on clicking on nav is fixed

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,20 +1,22 @@
 import React from 'react'
 import { Nav, Navbar, Container } from 'react-bootstrap'
+import { Link } from 'react-router-dom'
 
 const Header = () => {
   return (
     <header>
       <Navbar bg='dark' variant='dark' expand='lg' collapseOnSelect>
         <Container>
-          <Navbar.Brand href='/'>Basta</Navbar.Brand>
+          <Navbar.Brand as={Link} to='/'>
+            Basta
+          </Navbar.Brand>
           <Navbar.Toggle aria-controls='basic-navbar-nav' />
           <Navbar.Collapse id='basic-navbar-nav'>
             <Nav style={{ marginLeft: 'auto' }}>
-              <Nav.Link href='/cart'>
-                {' '}
+              <Nav.Link as={Link} to='/cart'>
                 <i className='fas fa-shopping-cart'></i> Cart
               </Nav.Link>
-              <Nav.Link href='/login'>
+              <Nav.Link as={Link} to='/login'>
                 <i className='fas fa-user'></i> Sign In
               </Nav.Link>
             </Nav>
