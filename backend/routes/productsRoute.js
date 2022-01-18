c107c323478d48cf304a2a51fe6041f530d1e330
Ankit kumar <ankitkr8540@gmail.com>
2022-01-18 12:36:33 +0530

    user Authentication endpoint done!!

--- backend/routes/productsRoute.js
+++ backend/routes/productsRoute.js
@@ -1,31 +1,12 @@
 import express from 'express'
-import AsyncHandler from 'express-async-handler'
 const router = express.Router()
-import Product from '../models/productModel.js'
+import {
+  getProducts,
+  getProductById,
+} from '../controllers/productController.js'
 
-//@desc Fetch all the products
-//@route GET /api/products
-//@access public
-router.get(
-  '/',
-  AsyncHandler(async (req, res) => {
-    const products = await Product.find({})
-    res.json(products)
-  })
-)
-//@desc Fetch single the products
-//@route GET /api/products/:id
-//@access public
-router.get(
-  '/:id',
-  AsyncHandler(async (req, res) => {
-    const product = await Product.findById(req.params.id)
-    if (product) {
-      res.json(product)
-    } else {
-      throw new Error('Product not Found')
-    }
-  })
-)
+router.route('/').get(getProducts)
+
+router.route('/:id').get(getProductById)
 
 export default router
