f2f8ced7eaada50031ad6610e9ac6e261f67e3e9
Ankit kumar <ankitkr8540@gmail.com>
2022-01-21 17:34:50 +0530

    checkout process part-1 done!

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,6 +10,7 @@ import RegisterScreen from './screens/RegisterScreen'
 import ProfileScreen from './screens/profileScreen'
 import ShippingScreen from './screens/ShippingScreen'
 import PaymentScreen from './screens/PaymentScreen'
+import PlaceOrderScreen from './screens/PlaceOrderScreen'
 function App() {
   return (
     <Router>
@@ -28,6 +29,7 @@ function App() {
             {/* Route for cart ends here */}
             <Route path='/shipping' element={<ShippingScreen />} />
             <Route path='/payment' element={<PaymentScreen />} />
+            <Route path='/placeorder' element={<PlaceOrderScreen />} />
           </Routes>
         </Container>
       </main>
