20f5dd04fe11cf12f8d10a13e6612858fd4e13a1
Ankit kumar <ankitkr8540@gmail.com>
2022-03-26 14:27:33 +0530

    admin controlling the user is done!

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -122,6 +122,44 @@ const deleteUser = asyncHandler(async (req, res) => {
     throw new Error('User Not found')
   }
 })
+
+//@desc Get user by id
+//@route GET /api/users/:id
+//@access private/Admin
+const getUserById = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.params.id).select('-password')
+  if (user) {
+    res.json(user)
+  } else {
+    res.status(404)
+    throw new Error('User Not found')
+  }
+})
+
+//@desc Update User
+//@route PUT /api/users/:id
+//@access private/Admin
+const updateUser = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.params.id)
+
+  if (user) {
+    user.name = req.body.name || user.name
+    user.email = req.body.email || user.email
+    user.isAdmin = req.body.isAdmin
+    const updatedUser = await user.save()
+
+    res.json({
+      _id: updatedUser._id,
+      name: updatedUser.name,
+      email: updatedUser.email,
+      isAdmin: updatedUser.isAdmin,
+    })
+  } else {
+    res.status(404)
+    throw new Error('User not found')
+  }
+})
+
 export {
   authUser,
   registerUser,
@@ -129,4 +167,6 @@ export {
   updateUserProfile,
   getUsers,
   deleteUser,
+  getUserById,
+  updateUser,
 }
